8da9b552c07cd4313c2bed2a536ef5c0ea70b700
Onurcan Genç <55115769+onurcangnc@users.noreply.github.com>
2023-05-22 14:11:14 +0300

    Add files via upload
    The project includes image processing so as to detect black tiles on each frame. Therefore, I just get the region of just 2 height. In a conclusion, the object needs low time for each object detection

---
+++ pianoGame.python.py
@@ -0,0 +1,50 @@
+import mss
+import numpy as np
+import cv2
+import pyautogui
+import time
+import keyboard
+
+pyautogui.PAUSE = 0.005
+
+def take_screenshot():
+    with mss.mss() as sct:
+        filename = sct.shot(output='fullscreen.png')
+    return filename        
+
+take_screenshot()
+
+def get_frame(region):
+    with mss.mss() as sct:
+        screen = np.array(sct.grab(region))
+        screen_grayscale = cv2.cvtColor(screen, cv2.COLOR_BGR2GRAY)
+        #print(screen_grayscale.shape)
+        cv2.imwrite('region.png', screen_grayscale)
+    return screen_grayscale
+
+def detect_tiles(frame):
+    for x in range(frame.shape[0]):
+        for y in range(frame.shape[1]):
+            if frame[x,y] == 0:
+                return x,y
+    return None
+
+region = {"top": 293, "left": 332, "width": 300, "height":2}
+
+
+time.sleep(3)
+
+while True:
+    if keyboard.is_pressed('q'):
+        break
+    start_time = time.time()
+    frame = get_frame(region)
+    coors = detect_tiles(frame)
+    if coors:
+        target_x = region['left'] + coors[1] + 1
+        target_y = region['top'] + coors[0] + 1
+        pyautogui.moveTo(x=target_x, y=target_y)
+        pyautogui.mouseDown()
+    print("%d FPS" % (1/(time.time() - start_time)))
+
+#print(coors)
